941ae06576b3ddd6319549f1935785902d3670ce
nirmalnyure1 <nirmalnyure1@gmail.com>
2021-09-05 14:58:37 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: Text(
+            'wellcomem to 30 day tutorial in flutter',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
